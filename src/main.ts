0469af5caff59f6a07a33ac5344f0dafd15fa3c9
Cameron Chamberlain <git@camjc.com>
2023-03-23 19:52:02 +1100

    [minor] third attempt

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function getAllFilenames(dirPath: string, fileArr: string[]) {
 
 function countByExtension(filenames: string[]) {
   return filenames.reduce((counts: {[x: string]: number}, filename: string) => {
-    const extension = filename.split('.')[1] || 'unknown';
+    const extension = path.extname(filename);
     return {...counts, [extension]: (counts[extension] || 0) + 1};
   }, {});
 }
