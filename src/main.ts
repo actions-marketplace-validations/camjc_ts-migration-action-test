08c26e43f778d622084a5e7a42b45c42c41243a6
Cameron Chamberlain <git@camjc.com>
2023-03-23 19:57:29 +1100

    [minor] third attempt

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,24 @@
-const core= require('@actions/core');
-const fs = require('fs').promises;
-const path= require('path');
+const core = require('@actions/core');
+const path = require('path');
+const { exec } = require("child_process");
 
-async function getAllFilenames(dirPath: string, fileArr: string[]) {
-  const files = await fs.readdir(dirPath);
-  for (const file of files) {
-    const filePath = path.join(dirPath, file);
-    const stats = await fs.stat(filePath);
-    if (stats.isDirectory()) {
-      await getAllFilenames(filePath, fileArr);
-    } else {
-      fileArr.push(filePath);
-    }
-  }
-  return fileArr;
+async function getAllFilenames(dirPath: string) {
+  exec("cd " + dirPath + " && git ls-files", (error, stdout, stderr) => {
+    return stdout.split('\r\n');
+  })
 }
 
 function countByExtension(filenames: string[]) {
-  return filenames.reduce((counts: {[x: string]: number}, filename: string) => {
+  return filenames.reduce((counts: { [x: string]: number }, filename: string) => {
     const extension = path.extname(filename);
-    return {...counts, [extension]: (counts[extension] || 0) + 1};
+    return { ...counts, [extension]: (counts[extension] || 0) + 1 };
   }, {});
 }
 
 async function run() {
   try {
     core.debug(`Hello from github runner`);
-    const arrayOfFiles = [];
-    await getAllFilenames(process.env.GITHUB_WORKSPACE || '/', arrayOfFiles);
+    const arrayOfFiles: string[] = (await getAllFilenames(process.env.GITHUB_WORKSPACE || '/') as any);
     core.setOutput('files-by-extension', countByExtension(arrayOfFiles));
   } catch (error) {
     core.setFailed((error as any).message);
